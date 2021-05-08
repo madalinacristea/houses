f32113574f8556a12c5d221e3e82b49b2eba2b52
madalinacristea <madalina.cristea11@gmail.com>
2021-05-08 09:49:58 +0100

    some changes

---
+++ src/Tile.js
@@ -0,0 +1,27 @@
+import Appointment from "./Appointment.js";
+import React, { useState } from "react";
+function Tile(props) {
+  const [view, setView] = useState(true);
+  if (view) {
+    return (
+      <div className='tile'>
+        <h1>
+          {props.area} {props.price} {props.type}
+        </h1>
+        <img alt="" src={props.image} />
+        <Appointment />
+        <button
+          onClick={() => {
+            setView(false);
+          }}
+        >
+          Hide
+        </button>
+      </div>
+    );
+  }
+  return(
+    <button onClick={()=>{setView(true)}}>Show</button>
+  )
+}
+export default Tile;
